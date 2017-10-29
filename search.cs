c856cda06d14af065a03ee15146a0a0d8aaa23fa
OneManDevelop <fzastahov@gmail.com>
2017-10-29 17:00:09 +0300

    untested search engine class added

---
+++ search.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace try1
+{
+    class Search
+    {
+        static void CountMatches(string[] inputs, string path, ref int max)
+        {
+            int i = 0;
+            max = 0;
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            XmlReader reader = XmlReader.Create(path, settings);
+            XmlWriter writer = XmlWriter.Create(path);
+
+            reader.MoveToContent();
+            // Parse the file and display each of the nodes.
+            while (reader.Read())
+            {
+                if(reader.Name == "reset")
+                {
+                    i = 0;
+                }
+                else
+                {
+                    foreach(string sympt in inputs)
+                    {
+                        if((sympt == reader.Value) && (reader.Name == "sympt"))
+                        {
+                            i++;
+                        }
+                    }
+                    if(reader.Name == "matches")
+                    {
+                        writer.WriteValue(i.ToString());
+                    }
+                    if (i > max)
+                    {
+                        max = i;
+                    }
+                }
+            }
+        }
+    }
+}
