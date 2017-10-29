05801f4be17a9cbbfd757b1166db53ac950339a9
OneManDevelop <fzastahov@gmail.com>
2017-10-29 16:29:29 +0300

    testing xml file - stable xml test

--- main.cs
+++ main.cs
@@ -36,7 +36,7 @@ namespace chatbot
             long dialogID = 199245750;                         
             char s;
 
-            XmlTextReader reader = new XmlTextReader(xmlpath); // testing feature
+            XmlTextReader readerr = new XmlTextReader(xmlpath); // testing feature
 
             FileStream file2 = new FileStream(path2, FileMode.OpenOrCreate);
             StreamReader reader2 = new StreamReader(file2);           
@@ -84,13 +84,54 @@ namespace chatbot
             }*/
 
 
-            while (reader.Read()) //testing feature
+            /*while (readerr.Read()) //testing feature
             {
-                Console.WriteLine(reader.Name);
+                Console.WriteLine(readerr.Name + " " + readerr.Value);
             }
-            Console.ReadLine();
+            Console.ReadLine();*/
 
 
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            XmlReader reader = XmlReader.Create(xmlpath, settings);
+
+            reader.MoveToContent();
+            // Parse the file and display each of the nodes.
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<{0}>", reader.Name);
+                        break;
+                    case XmlNodeType.Text:
+                        Console.Write(reader.Value);
+                        break;
+                    case XmlNodeType.CDATA:
+                        Console.Write("<![CDATA[{0}]]>", reader.Value);
+                        break;
+                    case XmlNodeType.ProcessingInstruction:
+                        Console.Write("<?{0} {1}?>", reader.Name, reader.Value);
+                        break;
+                    case XmlNodeType.Comment:
+                        Console.Write("<!--{0}-->", reader.Value);
+                        break;
+                    case XmlNodeType.XmlDeclaration:
+                        Console.Write("<?xml version='1.0'?>");
+                        break;
+                    case XmlNodeType.Document:
+                        break;
+                    case XmlNodeType.DocumentType:
+                        Console.Write("<!DOCTYPE {0} [{1}]", reader.Name, reader.Value);
+                        break;
+                    case XmlNodeType.EntityReference:
+                        Console.Write(reader.Name);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</{0}>", reader.Name);
+                        break;
+                }
+            }
 
             while (true)//start:loop
             {
