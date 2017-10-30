7533554284517429dcc34207aa95bdba12c52f0f
OneManDevelop <fzastahov@gmail.com>
2017-10-30 15:29:25 +0300

    output engine added - not tested

--- search.cs
+++ search.cs
@@ -8,28 +8,21 @@ namespace try1
 {
     class Search
     {
-        static void CountMatches(string[] inputs, ref string[] outputs, string path, ref int max)
+        static void CountMatches(string inputs, string path, ref int max)
         {
             int i = 0;
-            int k = 0;
-            int g;
             max = 0;
             XmlReaderSettings settings = new XmlReaderSettings();
             settings.DtdProcessing = DtdProcessing.Parse;
             XmlReader reader = XmlReader.Create(path, settings);
             XmlWriter writer = XmlWriter.Create(path);
 
-            for(g=0; g<=outputs.Length; g++)
-            {
-                outputs[g] = "";
-            }
-
             reader.MoveToContent();
             // Parse the file and display each of the nodes.
             while (reader.Read())
             {               
                 {
-                    foreach(string sympt in inputs)
+                    foreach(string sympt in inputs.Split(',',' '))
                     {
                         foreach(string val in reader.Value.Split(','))
                         {
@@ -43,6 +36,46 @@ namespace try1
                     {
                         max = i;
                     }
+                    i = 0; 
+                }
+            }
+        }
+        
+        static void FindOut(int max, ref string[] outputs, string inputs, string path)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            XmlReader reader = XmlReader.Create(path, settings);
+            XmlWriter writer = XmlWriter.Create(path);
+
+            int k = 0;
+            int g;
+            int i = 0;
+
+            for(g=0; g<outputs.Length; g++)
+            {
+                outputs[g] = "";
+            }
+
+            reader.MoveToContent();
+            // Parse the file and display each of the nodes.
+            while (reader.Read())
+            {
+                i = 0;
+                foreach (string sympt in inputs.Split(',', ' '))
+                {
+                    foreach (string val in reader.Value.Split(','))
+                    {
+                        if (sympt == val)
+                        {
+                            i++;
+                        }
+                    }
+                }
+                if (i == max)
+                {
+                    outputs[k] = reader.Name;
+                    k++;
                 }
             }
         }
